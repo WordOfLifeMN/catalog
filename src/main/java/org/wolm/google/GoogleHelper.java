227fb4c95d1198fa6e542b0aa452fb08052a5abc
wolm <wolm@192.168.0.10>
2015-06-03 01:16:25 -0500

    Update to OAuth2

--- src/main/java/org/wolm/google/GoogleHelper.java
+++ src/main/java/org/wolm/google/GoogleHelper.java
@@ -2,11 +2,14 @@ package org.wolm.google;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.Reader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.GeneralSecurityException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
@@ -14,6 +17,12 @@ import java.util.Properties;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson.JacksonFactory;
 import com.google.gdata.client.spreadsheet.SpreadsheetQuery;
 import com.google.gdata.client.spreadsheet.SpreadsheetService;
 import com.google.gdata.data.spreadsheet.SpreadsheetEntry;
@@ -24,23 +33,31 @@ import com.google.gdata.util.ServiceException;
 /**
  * Helps access data on the Google Drive.
  * <p>
- * To configure this by default, create a file ~/.wolm/googledrive.properties that contains:
+ * To configure this by default, create a file ~/.wolm/google.properties that contains:
  * <ul>
- * <li>username=
- * <li>password=
+ * <li>serviceAccountId=
  * </ul>
- * If this file exists, this class will be initialized with the values found therein. If this file does not exist, then
- * you must configure this object manually with {@code setUserName()} and {@code setPassword()} before using it.
+ * The Service Account ID is the "Email address" of the service account from the <a
+ * href="https://console.developers.google.com/project">Developers Console</a>. The spreadsheets must be shared with
+ * this account
+ * <p>
+ * The directory ~/.wolm must also contain the P12 key file for the service account
+ * <p>
+ * If these files exist, this class will be initialized with the values found therein. If these files do not exist, then
+ * you must configure this object manually with {@code setServiceAccountId()} and {@code setP12KeyFile()} before using
+ * it.
  * 
  * @author wolm
  */
 public class GoogleHelper {
 	private final String applicationName;
 	private final URL feedUrl;
+	private final JsonFactory JSON_FACTORY = new JacksonFactory();
+	private final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
 
 	private SpreadsheetService service;
-	private String userName;
-	private String password;
+	private String serviceAccountId;
+	private File p12KeyFile;
 
 	public GoogleHelper(String applicationName) {
 		super();
@@ -59,23 +76,36 @@ public class GoogleHelper {
 		feedUrl = feed;
 	}
 
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public String getServiceAccountId() {
+		return serviceAccountId;
+	}
+
+	public void setServiceAccountId(String serviceAccountId) {
+		this.serviceAccountId = serviceAccountId;
 		service = null;
 	}
 
-	public void setPassword(String password) {
-		this.password = password;
+	public File getP12KeyFile() {
+		return p12KeyFile;
+	}
+
+	public void setP12KeyFile(File p12KeyFile) {
+		this.p12KeyFile = p12KeyFile;
 		service = null;
 	}
 
 	/** @return The spreadsheet service installed with WOLM credentials */
 	SpreadsheetService getService() throws AuthenticationException {
-		if (service == null) {
-			service = new SpreadsheetService(applicationName);
-			service.setUserCredentials(userName, password);
+		try {
+			if (service == null) {
+				service = new SpreadsheetService(applicationName);
+				service.setOAuth2Credentials(getOAuth2Credentials());
+			}
+			return service;
 		}
-		return service;
+		catch (GeneralSecurityException | IOException e) {
+			throw new AuthenticationException("Unable to authenticate with Google due to: " + e.getMessage());
+		}
 	}
 
 	/** @return List of all spreadsheets that the user has access to */
@@ -120,21 +150,37 @@ public class GoogleHelper {
 	}
 
 	/**
-	 * Configures this class from a properties file on disk
+	 * Configures this class from files in the ~/.wolm directory
 	 * 
 	 */
 	private void configure() {
-		File propertiesFile = new File(System.getenv("HOME") + "/.wolm/googledrive.properties");
-		if (!propertiesFile.exists()) return;
-
-		try (Reader reader = new FileReader(propertiesFile)) {
-			Properties properties = new Properties();
-			properties.load(reader);
-			setUserName(properties.getProperty("username"));
-			setPassword(properties.getProperty("password"));
+		// read properties
+		File propertiesFile = new File(System.getenv("HOME") + "/.wolm/google.properties");
+		if (propertiesFile.exists()) {
+			try (Reader reader = new FileReader(propertiesFile)) {
+				Properties properties = new Properties();
+				properties.load(reader);
+				setServiceAccountId(properties.getProperty("serviceAccountId"));
+			}
+			catch (IOException e) {
+				// just ignore, the class must be manually configured
+			}
 		}
-		catch (IOException e) {
-			// just ignore, the class must be manually configured
+
+		// find the p12 key - assume there is only one
+		File configurationDir = new File(System.getenv("HOME") + "/.wolm");
+		File[] p12KeyFiles = configurationDir.listFiles(new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.endsWith(".p12");
+			}
+		});
+
+		if (p12KeyFiles.length > 0) {
+			if (p12KeyFiles.length > 1) {
+				System.out
+						.println("WARNING: Multiple P12 Key Files were found, using the first one: " + p12KeyFiles[0]);
+			}
+			setP12KeyFile(p12KeyFiles[0]);
 		}
 	}
 
@@ -145,9 +191,26 @@ public class GoogleHelper {
 	 * @throws IllegalStateException if it is not configured correctly
 	 */
 	private boolean isConfigured() {
-		if (userName == null) throw new IllegalStateException("GoogleHelper has no user name configured");
-		if (password == null) throw new IllegalStateException("GoogleHelper has no password configured");
+		if (getServiceAccountId() == null) throw new IllegalStateException(
+				"GoogleHelper has no service account ID configured");
+		if (getP12KeyFile() == null) throw new IllegalStateException("GoogleHelper could not find a P12 Key File");
 		return true;
 	}
 
+	/**
+	 * Creates a set of OAuth2 credentials needed to connect to Google API
+	 * 
+	 * @return
+	 * @throws GeneralSecurityException
+	 * @throws IOException
+	 */
+	private Credential getOAuth2Credentials() throws GeneralSecurityException, IOException {
+		GoogleCredential.Builder builder = new GoogleCredential.Builder();
+		builder.setTransport(HTTP_TRANSPORT);
+		builder.setJsonFactory(JSON_FACTORY);
+		builder.setServiceAccountId(getServiceAccountId());
+		builder.setServiceAccountScopes(Arrays.asList("https://spreadsheets.google.com/feeds"));
+		builder.setServiceAccountPrivateKeyFromP12File(getP12KeyFile());
+		return builder.build();
+	}
 }
