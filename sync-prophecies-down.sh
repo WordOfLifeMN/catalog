e265ad707860ae15b6fdce1221b63b11612b04ac
Kevin Murray <kevmurray@icloud.com>
2020-02-22 11:28:30 -0600

    Download the prophecy directory recursively

--- sync-prophecies-down.sh
+++ sync-prophecies-down.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aws s3 sync s3://wordoflife.mn.catalog/prophecy ~/tmp/catalog/prophecy
-
+# aws s3 sync s3://wordoflife.mn.catalog/prophecy ~/tmp/catalog/prophecy
+aws s3 cp --recursive s3://wordoflife.mn.prophecy ~/tmp/catalog/prophecy
